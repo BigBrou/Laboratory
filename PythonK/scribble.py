3cc21c9f33a9ddf5f0c365ee628a9061143b32a2
kangchoi21 <kangchoi21@gmail.com>
2018-08-02 13:10:10 +0900

    Delete
    Delete

--- PythonK/scribble.py
+++ PythonK/scribble.py
@@ -9,6 +9,16 @@ dic = {'aa':[1,2,3]}
 
 print(dic['aa'][2])
 print(dic.get('aa')[2])
-'''
+
 import datetime
 datetime.datetime.strptime('2016-01-02', '%Y-%m-%d')
+'''
+from matplotlib import pyplot as plt
+import numpy as np
+import seaborn as sns
+
+np.random.seed(0)
+uniform_data = np.random.rand(10, 12)
+print(uniform_data)
+sns.heatmap(uniform_data)
+plt.show()
