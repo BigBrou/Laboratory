c45f37ecd2701a67f460b7eb2ab4e57ac4c82cd3
kangchoi21 <kangchoi21@gmail.com>
2018-11-28 14:19:05 +0900

    Coding Stamp Learn 6.Variable

---
+++ PythonK/CodingStamp/6.Variable/Learning.py
@@ -0,0 +1,26 @@
+#Assign Variable
+x, y, z = 10, 20, 30
+a = b = c = 40
+
+print(x, y, z)
+print(a, b, c)
+
+#Swap Variable
+x, y = y, x
+print(x, y, z)
+
+#Delete Variable
+del x, y, z
+
+#Assign null nullType Data
+x = None
+print(x, type(x))
+
+#Assign & Calculate at Once
+num = 90
+
+num += 10
+num *= 20
+num /= 5
+num //= 2
+print(num)
