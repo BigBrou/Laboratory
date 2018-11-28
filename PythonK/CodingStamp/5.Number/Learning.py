f6ac94a5f445fc16aab3e2174b6131d4f4466919
kangchoi21 <kangchoi21@gmail.com>
2018-11-28 11:23:05 +0900

    Learn CodingStamp chapter5

--- PythonK/CodingStamp/5.Number/Learning.py
+++ PythonK/CodingStamp/5.Number/Learning.py
@@ -1,4 +1,11 @@
+#divmod
 quotient, remainder = divmod(5, 2)
-
 print('quotient ', quotient)
 print('remainder', remainder)
+print()
+
+#진수 표현
+binary = 0b110
+octaldecimal = 0o100
+hexadecimal  = 0xA2
+print(binary, octaldecimal, hexadecimal)
